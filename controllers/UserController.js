a96189f2f4e7e2220b87a5fa34f7fe825f404d6f
Gautam Shah <gautamshah317@gmail.com>
2025-01-15 13:13:06 +0530

    implemeted the error handling

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,68 +1,81 @@
 import UserModel from "../models/UserModel.js";
 
 // fetch all the registered users
-const fetchUser=async(req,res)=>{
-    try {
-        const users=await UserModel.find();
-        res.status(200).json({
-            success:true,
-            users:users
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            success:false,
-            message:"Internal Server Error"
-        })
-    }
-}
+const fetchUser = async (req, res) => {
+  try {
+    const users = await UserModel.find();
+    res.status(200).json({
+      success: true,
+      users: users,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
 
 // create new user
-const createUser=async(req,res)=>{
-    try {
-        const {name,age,hobbies}=req.body;
-        const newUser=await UserModel.create({name,age,hobbies});
-        res.status(201).json({
-            success:true,
-            message:"User Created Successfully",
-            newUser:newUser
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            success:false,
-            message:"Internal Server Error"
-        })
-    }
-}
+const createUser = async (req, res) => {
+  try {
+    const { name, age, hobbies } = req.body;
+    const newUser = await UserModel.create({ name, age, hobbies });
+    res.status(201).json({
+      success: true,
+      message: "User Created Successfully",
+      newUser: newUser,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
 
 // update the existing user
-const updateUser=async(req,res)=>{
-    try {
-        const {id}=req.params;
-        const {name,age,hobbies}=req.body;
-        const user=await UserModel.findByIdAndUpdate(id,{name,age,hobbies},{new:true});
-        res.status(200).json({
-            success:true,
-            message:"User Details Updated Successfully",
-            updatedUser:user
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            success:false,
-            message:"Internal Server Error"
-        })
-    }
-}
+const updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, age, hobbies } = req.body;
+    const user = await UserModel.findByIdAndUpdate(
+      id,
+      { name, age, hobbies },
+      { new: true }
+    );
+    res.status(200).json({
+      success: true,
+      message: "User Details Updated Successfully",
+      updatedUser: user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
 
 // delete the user
-const deleteUser=async(req,res)=>{
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await UserModel.findByIdAndDelete(id);
+    res.status(200).json({
+      success: true,
+      message: "User Deleted Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
 
-export {fetchUser,createUser,updateUser,deleteUser}
+export { fetchUser, createUser, updateUser, deleteUser };
